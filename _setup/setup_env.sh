39135fc9cef0003b3a319a29bbf17d9578b8263b
Ayoma Wijethunga <ayomawdb@gmail.com>
2020-01-15 23:46:35 +0530

    Break setup script into parts

--- _setup/setup_env.sh
+++ _setup/setup_env.sh
@@ -1,14 +1,14 @@
-sudo apt -y update
-#sudo apt -y upgrade
-sudo apt -y install git
-sudo apt -y install unzip
-sudo apt -y install unrar
+#sudo apt -y update
+##sudo apt -y upgrade
+#sudo apt -y install git
+#sudo apt -y install unzip
+#sudo apt -y install unrar
 
-sudo apt -y install exploitdb
-#sudo apt -y install metasploit-framework
+#sudo apt -y install exploitdb
+##sudo apt -y install metasploit-framework
 
-searchsploit -u
-#msfupdate
+#searchsploit -u
+##msfupdate
 
 # https://github.com/weaknetlabs/Penetration-Testing-Grimoire/blob/master/Vulnerabilities/SSH/key-exploit.md
 sudo echo "PubkeyAcceptedKeyTypes +ssh-dss" >> /etc/ssh/ssh_config
--- setup.sh
+++ setup.sh
@@ -1,24 +1,36 @@
 #!/bin/bash
 
+export ip_local='10.10.10.10'
+export port_local=443
+export port_remote=65300
+
 bash _setup/clean.sh
 read -n 1 -s -r -p "clean complete. Press any key to continue"
+echo ""
 # Do environment changes firest (update pkg lists, etc.)
 bash _setup/setup_env.sh
 read -n 1 -s -r -p "setup_env complete. Press any key to continue"
+echo ""
 # Payloaf, fuzzing, and other lists
 bash _setup/setup_lists.sh
 read -n 1 -s -r -p "setup_lists complete. Press any key to continue"
+echo ""
 # Exploits usable to gain initial foothold
 bash _setup/setup_exploits_*.sh
 read -n 1 -s -r -p "setup_exploits_* complete. Press any key to continue"
+echo ""
 # Different attack payloads (setup_payloads.sh will be called internally)
 bash _setup/setup_payloads_*.sh
 read -n 1 -s -r -p "setup_payloads_* complete. Press any key to continue"
+echo ""
 # Scripts that need to be accessed from victim host
 bash _setup/setup_scripts_*.sh
 read -n 1 -s -r -p "setup_scripts_* complete. Press any key to continue"
+echo ""
 # DIfferent tools used locally (in attacker's machine)
 bash _setup/setup_tools.sh
 read -n 1 -s -r -p "setup_tools complete. Press any key to continue"
+echo ""
 bash _setup/setup_tools_*.sh
 read -n 1 -s -r -p "setup_tools_* complete. Press any key to continue"
+echo ""
