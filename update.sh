07b46bad422cbe81f2eebde5bf74970ba95c7b13
Ayoma Wijethunga <ayomawdb@gmail.com>
2020-03-27 15:04:37 +0530

    Fix update

--- update.sh
+++ update.sh
@@ -5,16 +5,20 @@ if [ ! $# -eq 1 ]
   then
     echo "Usage ./update.sh remote_port"
     echo ""
+    echo "  source_ip   - IP address or the interface-name (ex: tun0) of the attacker machine"
+    echo "                This is mostly used as the target IP of reverse-tcp connections"
+    echo ""
+    echo "  source_port - Port to listen on the attacker machine"
+    echo "                This is mostly used as the destination port of reverse-tcp connections"
+    echo ""
     echo "  remote_port - Port to open on the target machine"
     echo "                This is mostly used as the source port of bind-tcp connections"
     echo ""
     exit
 fi
 
-source_ip="`cat _setup/config.properties | grep "source_ip" | cut -d '=' -f2`"
-source_port="`cat _setup/config.properties | grep "source_port" | cut -d '=' -f2`"
-remote_port="`cat _setup/config.properties | grep "remote_port" | cut -d '=' -f2`"
-
-remote_port=$1
+source_ip=$1
+source_port=$2
+remote_port=$3
 
 bash setup.sh $source_ip $source_port $remote_port false true
