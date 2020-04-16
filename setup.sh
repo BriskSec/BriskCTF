5d845e5ffb8696f757cef78ae95e61c125c9f654
Ayoma Wijethunga <ayomawdb@gmail.com>
2020-04-16 12:51:59 +0530

    New updates

--- setup.sh
+++ setup.sh
@@ -120,19 +120,17 @@ if $updateOnly; then
     exit
 fi 
 
-if [ ! $updateOnly ] ; then 
-    header "Cleanup tasks"
-    bash _setup/clean.sh
-    cd "$setup_home"
+header "Cleanup tasks"
+bash _setup/clean.sh
+cd "$setup_home"
 
-    header "Environment setup"
-    bash _setup/setup_env.sh
-    cd "$setup_home"
+header "Environment setup"
+bash _setup/setup_env.sh
+cd "$setup_home"
 
-    header "Lists - Payloaf, fuzzing, and other lists"
-    bash _setup/setup_lists.sh
-    cd "$setup_home"
-fi
+header "Lists - Payloaf, fuzzing, and other lists"
+bash _setup/setup_lists.sh
+cd "$setup_home"
 
 header "Payloads - Different attack payloads"
 for i in _setup/setup_payloads_*.sh; do bash $i; cd "$setup_home"; done
@@ -140,15 +138,14 @@ for i in _setup/setup_payloads_*.sh; do bash $i; cd "$setup_home"; done
 header "Exploits - Exploits usable to gain initial foothold & prevesc"
 for i in _setup/setup_exploits_*.sh; do bash $i; cd "$setup_home"; done
 
-if [ ! $updateOnly ] ; then 
-    header "Tools - Different tools used locally (in attacker's machine)"
-    bash _setup/setup_tools.sh
-    for i in _setup/setup_tools_*.sh; do bash $i; cd "$setup_home"; done
+header "Tools - Different tools used locally (in attacker's machine)"
+bash _setup/setup_tools.sh
 
-    header "Public - Scripts or tools that need to be accessed from victim host"
-    for i in _setup/setup_public_*.sh; do bash $i; cd "$setup_home"; done
+for i in _setup/setup_tools_*.sh; do bash $i; cd "$setup_home"; done
 
-    banner "Updating: mlocate database" 
-    updatedb
-fi
+header "Public - Scripts or tools that need to be accessed from victim host"
+for i in _setup/setup_public_*.sh; do bash $i; cd "$setup_home"; done
+
+banner "Updating: mlocate database" 
+updatedb
 
